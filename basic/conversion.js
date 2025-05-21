5dc4a7ae370ff35662905ea0d2a66c64c005d4d9
MKiKiUser <165588207+MKiKiUser@users.noreply.github.com>
2025-05-21 06:21:06 +0000

    Conversion

---
+++ basic/conversion.js
@@ -0,0 +1,32 @@
+
+// let score ="33acd"
+// let a =undefined
+// console.log(typeof score);
+// let valueInnumber = Number(score)
+// console.log(valueInnumber)
+// console.log(typeof(a));
+// let b =true;
+// console.log(typeof b)
+//"33" => 33
+//"33abc" => NaN
+//null => 0
+// ""=> false
+// let isLoggedIn= 1
+// let booleanIsLoggedIn =  Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// ****** Operations *******
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+console.log(2+true)
+console.log(+true)
+//console.log(true+) // error
+console.log(+"")
+let gameCounter = 100
+++gameCounter // game ++
+console.log(gameCounter)
+
+
+
