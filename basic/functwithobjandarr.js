a2536ebc356150af28d5046a6512baa9562bf6ab
MKiKiUser <165588207+MKiKiUser@users.noreply.github.com>
2025-05-25 15:02:26 +0000

    functionobject

---
+++ basic/functwithobjandarr.js
@@ -0,0 +1,28 @@
+function calculateCarprice(v1,v2,...n1)//rest operator
+{
+ return n1
+}
+
+//console.log(calculateCarprice(2,3,4))
+const user ={
+    name:"sonali",
+    p1:199
+}
+function handleObject(anyObject){
+    console.log(`user is ${anyObject.name} abd p is ${anyObject.p}`)
+}
+//handleObject(user)
+
+handleObject({
+    name:"sona",
+    p:399
+
+})
+
+const mynewarr = [2,3,4,5]
+function returnSe(getArray){
+    return getArray[1]
+
+}
+console.log(returnSe(mynewarr))
+//or([1,2,3,4])
