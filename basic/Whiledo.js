dee25ec957063bd11176d575f663fef94c12c0e1
MKiKiUser <165588207+MKiKiUser@users.noreply.github.com>
2025-05-31 07:43:53 +0000

    Whileanddowhile

---
+++ basic/Whiledo.js
@@ -0,0 +1,21 @@
+let index = 0
+while (index <=10) {
+    console.log(`Value of index is ${index}`)
+    index = index+2
+    
+}
+
+let myarray =["flash","batmab","super"]
+let arr =0
+while (arr<myarray.length)
+{
+    console.group(`Value is ${myarray[arr]}`)
+    arr=arr+1
+}
+
+//do while
+let s=11
+do{
+console.log(`s is ${s}`);
+s++ 
+}while(s<=10)
